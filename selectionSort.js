47c98bac0f9945097f394d69f3d59da446c23af4
levineye <levineye@gmail.com>
2022-03-08 23:40:39 +0300

    feat: update insertion sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,20 +1,17 @@
 function swap(arr, left, right) {
   [arr[left], arr[right]] = [arr[right], arr[left]];
 }
-
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
 
-    for (let j = i; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex] || minIndex === undefined) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
         minIndex = j;
       }
     }
 
-    if (i !== minIndex) {
-      swap(arr, i, minIndex);
-    }
+    swap(arr, i, minIndex);
   }
 
   return arr;
